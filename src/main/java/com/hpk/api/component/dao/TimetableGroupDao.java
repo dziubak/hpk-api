d8ba72f8ff83299887f189c734667c946a7f8f0c
vlad.dziubak <vlad.dziubak@upholding.biz>
2019-06-03 02:17:34 +0300

    Some change for timetable api for group

--- src/main/java/com/hpk/api/component/dao/TimetableGroupDao.java
+++ src/main/java/com/hpk/api/component/dao/TimetableGroupDao.java
@@ -20,6 +20,8 @@ public class TimetableGroupDao {
 
 	public List<TimetableGroup> getGroupTimetableById(int groupId, String position, DayOfWeek dayOfWeek) {
 		String dayOfWeekCondition = dayOfWeek != null ? " AND day_of_week=:dayOfWeek" : "";
+		String positionCondition = " AND tt.position='повний'";
+		String positionFullContidion = position != null ? " AND (tt.position=:position OR  tt.position='повний')" : positionCondition;
 
 		String sql = "SELECT tt.day_of_week as dayOfWeek, tt.number_of_couple as numberOfCouple, " +
 				"tt.position as position, g.name as groupName, " +
@@ -33,7 +35,7 @@ public class TimetableGroupDao {
 				"LEFT JOIN classroom c1 ON tt.classroom_id = c1.id " +
 				"LEFT JOIN classroom c2 ON tt.classroom_second_id = c2.id " +
 				"LEFT JOIN subject s ON tt.subject_id = s.id " +
-				"WHERE (g.id=:groupId) AND tt.position=:position " + dayOfWeekCondition;
+				"WHERE (g.id=:groupId) " + positionFullContidion + dayOfWeekCondition;
 
 		Map<String, Object> params = new HashMap<>();
 		params.put("groupId", groupId);
